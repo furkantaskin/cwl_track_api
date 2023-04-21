4e210891caef4febd0c90a144aecd7f1b0dfe7f2
Furkan Taşkın <furkan.taskin@tuta.io>
2023-04-21 22:07:23 +0300

    Create basic get method for services

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
 from fastapi import FastAPI
+from typing import Dict, List
 
 app = FastAPI()
+
+service_dict = Dict[str, int | bool]
+services_list = List[service_dict]
+
+
+service_list: Dict[str, services_list] = {
+    "services": [
+        {
+            "name": "crater",
+            "url": "https://crater.com.tr",
+            "type": "domain",
+            "startedAt": "2021-01-01",
+            "finishAt": "2024-01-01",
+            "price": 1000,
+            "paid": "False",
+            "currency": "TRY",
+            "customer": "Özal Acar"
+        },
+        {
+            "name": "basovali",
+            "url": "https://basovali.com",
+            "type": "hosting",
+            "startAt": "2021-01-01",
+            "finishAt": "2023-01-01",
+            "price": 300,
+            "paid": True,
+            "currency": "EUR",
+            "customer": "Emin Göroğlu"
+        }
+    ]
+}
 
 
 @app.get("/")
@@ -11,5 +43,9 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("/services")
+async def get_services():
+    return service_list
 
 

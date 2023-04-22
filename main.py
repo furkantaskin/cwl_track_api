0c860823a2683dba64865c10d103672a88a5493b
Furkan Taşkın <furkan.taskin@tuta.io>
2023-04-23 00:02:31 +0300

    Move classes and service list to files - Move models to classes module - Move dictionary to json - Move file operations to services module

--- main.py
+++ main.py
@@ -1,38 +1,7 @@
+from app.services import read_services, read_customers
 from fastapi import FastAPI
-from typing import Dict, List
 
 app = FastAPI()
-
-service_dict = Dict[str, int | bool]
-services_list = List[service_dict]
-
-
-service_list: Dict[str, services_list] = {
-    "services": [
-        {
-            "name": "crater",
-            "url": "https://crater.com.tr",
-            "type": "domain",
-            "startedAt": "2021-01-01",
-            "finishAt": "2024-01-01",
-            "price": 1000,
-            "paid": "False",
-            "currency": "TRY",
-            "customer": "Özal Acar"
-        },
-        {
-            "name": "basovali",
-            "url": "https://basovali.com",
-            "type": "hosting",
-            "startAt": "2021-01-01",
-            "finishAt": "2023-01-01",
-            "price": 300,
-            "paid": True,
-            "currency": "EUR",
-            "customer": "Emin Göroğlu"
-        }
-    ]
-}
 
 
 @app.get("/")
@@ -40,12 +9,13 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-@app.get("/services")
+@app.get("/get-services")
 async def get_services():
-    return service_list
+    return read_services()
+
+
+@app.get("/get-customers")
+async def get_users():
+    return read_customers()
 
 

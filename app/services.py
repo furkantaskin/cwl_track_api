d4cf2888f184a6ad42ee0e376e27c739b2bdbfc3
Furkan Taşkın <furkan.taskin@tuta.io>
2023-04-22 21:51:48 +0300

    Add UTF-8 encoding for file operation

--- app/services.py
+++ app/services.py
@@ -4,7 +4,7 @@ import json
 
 def read_services() -> ServiceList:
     # Read services from json file
-    with open("res/services.json", "r") as f:
+    with open("res/services.json", "r", encoding="utf-8") as f:
         service_list = json.load(f)
     return service_list
 
